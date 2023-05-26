0cc69d9e96d053531227e6c8b6e1552582daa259
Maya Magavi <maya_magavi@brown.edu>
2023-05-26 11:25:33 -0400

    main

---
+++ Main.java
@@ -0,0 +1,11 @@
+import javax.swing.JFrame;
+
+/**
+* Main class runs JFrame
+ */
+public class Main {
+    public static void main(String[] args) {
+        JFrame window = new JFrame();
+
+    }
+}
